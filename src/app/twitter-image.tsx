6bd998a674369e46037c270dd0d741f01fa3c2d4
Anubhavkumar931 <anubhavkumar9310@gmail.com>
2026-02-17 14:31:53 +0530

    Configure GitHub Pages deployment for Next.js static export

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -2,6 +2,8 @@ import { ImageResponse } from "next/og";
 
 import { siteConfig } from "@/content/profile";
 
+export const dynamic = "force-static";
+
 export const size = {
   width: 1200,
   height: 600,
